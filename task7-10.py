2698f43af30a9f70e46c2282ed8a6d8f26792dc6
Ekaterina-swisslaska <swisslaska@yandex.ru>
2022-01-24 23:04:44 +0300

    task 7-10

---
+++ task7-10.py
@@ -0,0 +1,11 @@
+my_list = [42, 43]
+my_dict = {	'foo': {'a':12, 'b': (1, 2, 3, 4, my_list)},
+			   'bar': {'c':12, 'd': {5, 6, 7, 8}},
+	'moo': {'e':12, 'f': {'Lol': ['L', 'o', 'l']}},}
+value_foo = my_dict.get("foo")
+print(unique_values)
+my_list.pop(0)
+print(my_list)
+my_dict = { 'f': {'Lol': ['L', 'o', 'l']}}
+my_dict['K'] = ['K', 'e', 'k']
+print(my_dict)
